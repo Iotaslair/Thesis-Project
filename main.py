893a795e3bced7367da51e49fa0615d8dcd7adfb
Iotaslair <35147738+Iotaslair@users.noreply.github.com>
2020-11-07 14:50:23 -0500

    Made vars for number of people to create and team size

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pims
 import itertools
+number_of_people_to_create = 5
+team_size = 3
 
 people = []
-for i in range(4):
+
+for i in range(number_of_people_to_create):
     people.append(pims.make_person())
 
-teams = itertools.combinations(people, 3)
+teams = itertools.combinations(people, team_size)
+
 best_team = []
 best_team_score = 0
 for team in teams:
