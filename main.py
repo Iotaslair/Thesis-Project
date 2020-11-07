3b4c3b17dc86265d81ffe0d1fea3efcc7cdd2467
Iotaslair <35147738+Iotaslair@users.noreply.github.com>
2020-11-07 14:50:01 -0500

    Wrote code for going through all teams and getting the highest scored one

--- main.py
+++ main.py
@@ -6,4 +6,13 @@ for i in range(4):
     people.append(pims.make_person())
 
 teams = itertools.combinations(people, 3)
+best_team = []
+best_team_score = 0
+for team in teams:
+    if (test(team) > best_team_score):
+        best_team_score = test(team)
+        best_team = team
+
+print("Most Compatible Team: " + str(best_team))
+print("Score of best team: " + best_team_score)
 
