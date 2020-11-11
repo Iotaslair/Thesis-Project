642e7fb5d14805f55d560274c54b45a2eeeafd60
Iotaslair <35147738+Iotaslair@users.noreply.github.com>
2020-11-10 20:11:48 -0500

    Added code to sort and organize teams that have been tested

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pims
 import itertools
+import tester
+
 number_of_people_to_create = 5
 team_size = 3
 
@@ -11,12 +13,27 @@ for i in range(number_of_people_to_create):
 teams = itertools.combinations(people, team_size)
 
 best_team = []
-best_team_score = 0
+best_team_score = 10000
+
+best_team_dict = {}
+
 for team in teams:
-    if (test(team) > best_team_score):
-        best_team_score = test(team)
+    score = tester.test(team, team_size)
+    best_team_dict[score] = team
+    if score < best_team_score:
+        best_team_score = score
         best_team = team
 
-print("Most Compatible Team: " + str(best_team))
-print("Score of best team: " + best_team_score)
+# Get out all scores, sort them, then use the best scores from the dict to print the teams
+# This is used to get the best teams
+scores = []
+for score in best_team_dict:
+    scores.append(score)
+scores = sorted(scores)
 
+best_team_string = ""
+for person in best_team:
+    best_team_string += str(person) + "\n"
+
+print("Score of best team: " + str(best_team_score))
+print("Most Compatible Team: \n" + best_team_string)
