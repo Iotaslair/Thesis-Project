6f62ded35792007c84673ccdb5f67cab37c48569
Iotaslair <35147738+Iotaslair@users.noreply.github.com>
2020-11-12 10:55:22 -0500

    Added more comments to the code to make it easier to understand

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ teams = itertools.combinations(people, team_size)
 
 best_team_dict = {}
 
+# Tests the teams and stores the scores in a dictionary {score, team}
 for team in teams:
     score = tester.test(team, team_size)
     best_team_dict[score] = team
 
-# Get out all scores, sort them, then use the best scores from the dict to print the teams
-# This is used to get the best teams
+# Sorts the teams based on their scores
 scores = []
 for score in best_team_dict:
     scores.append(score)
 scores = sorted(scores)
 
+# Printing the best teams and a little description
 print("A lower score is better because it is considered to be closer to the perfect team")
 print("A score of 0 is considered to be the perfect team")
 for i in range(3):
